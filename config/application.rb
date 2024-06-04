83220254b452b771e662ce428dbb0a941cd970a8
root <root@Y>
2024-06-04 22:11:55 +0900

    1-5.1

--- config/application.rb
+++ config/application.rb
@@ -9,8 +9,8 @@ module JITM
     config.load_defaults 6.1
 
     # デフォルトの言語を日本語に設定
-   # config.i18n.default_locale = :ja
-    config.i18n.default_locale = :en
+    config.i18n.default_locale = :ja
+   # config.i18n.default_locale = :en
     # 利用可能なロケールを設定（必要に応じて）
     config.i18n.available_locales = [:en, :ja]
   end
